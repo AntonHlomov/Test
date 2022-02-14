8348c18a0576058aa4a3ae47e1d67e12f98ca5e5
Khlomov Anton <antonhlomov@yandex.ru>
2022-02-14 10:46:55 +0100

    поменял ссылку на первое фото

--- Test/Builder/Builder.swift
+++ Test/Builder/Builder.swift
@@ -14,7 +14,7 @@ protocol Builder{
 class ModelBuilder: Builder{
     
     static func mainModule() -> UIViewController {
-        let dataLinks =  ["https://picsum.photos/id/1070/800","https://picsum.photos/id/228/800","https://picsum.photos/id/281/800","https://picsum.photos/id/314/800","https://picsum.photos/id/258/800","https://picsum.photos/id/238/800"]
+        let dataLinks =  ["https://picsum.photos/id/133/800","https://picsum.photos/id/228/800","https://picsum.photos/id/281/800","https://picsum.photos/id/314/800","https://picsum.photos/id/258/800","https://picsum.photos/id/238/800"]
         let view = ImagesController()
         let networkService = NetworkService()
         let presenter = ImagesPresenter(view: view, networkService: networkService,dataLinks: dataLinks)
